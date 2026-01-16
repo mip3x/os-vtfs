b1b6ee099ebeb03e357df467b7e6e3df4ce3ad8b
mip3x <plombiiiri@yandex.ru>
2026-01-16 22:40:06 +0300

    chore: indent changed

--- vtfs/source/http.h
+++ vtfs/source/http.h
@@ -3,10 +3,15 @@
 
 #include <linux/inet.h>
 
-int64_t vtfs_http_call(const char *token, const char *method,
-                            char *response_buffer, size_t buffer_size,
-                            size_t arg_size, ...);
+int64_t vtfs_http_call(
+    const char *token,
+    const char *method,
+    char* response_buffer,
+    size_t buffer_size,
+    size_t arg_size,
+    ...
+);
 
-void encode(const char *, char *);
+void encode(const char*, char*);
 
-#endif // VTFS_HTTP_H
+#endif  // VTFS_HTTP_H
--- vtfs/source/vtfs.c
+++ vtfs/source/vtfs.c
@@ -11,12 +11,12 @@ MODULE_DESCRIPTION("A simple FS kernel module");
 #define LOG(fmt, ...) pr_info("[" MODULE_NAME "]: " fmt, ##__VA_ARGS__)
 
 static int __init vtfs_init(void) {
-  LOG("VTFS joined the kernel\n");
-  return 0;
+    LOG("VTFS joined the kernel\n");
+    return 0;
 }
 
 static void __exit vtfs_exit(void) {
-  LOG("VTFS left the kernel\n");
+    LOG("VTFS left the kernel\n");
 }
 
 module_init(vtfs_init);
