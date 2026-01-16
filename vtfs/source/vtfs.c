8b65234f6cbd3860959904ded8102cf86a0590d7
mip3x <plombiiiri@yandex.ru>
2026-01-17 00:57:32 +0300

    feat: vtfs_iterate added but doesn't work yet; task 3 done

--- vtfs/source/vtfs.c
+++ vtfs/source/vtfs.c
@@ -18,6 +18,7 @@ static void vtfs_kill_sb(struct super_block*);
 static int vtfs_fill_super(struct super_block*, void*, int);
 static struct inode* vtfs_get_inode(struct super_block*, const struct inode*, umode_t, int);
 static struct dentry* vtfs_lookup(struct inode* parent_inode, struct dentry* child_dentry, unsigned int flag);
+static int vtfs_iterate(struct file* filp, struct dir_context* ctx);
 
 struct file_system_type vtfs_fs_type = {
     .name = "vtfs",
@@ -27,6 +28,10 @@ struct file_system_type vtfs_fs_type = {
 
 struct inode_operations vtfs_inode_ops = {
     .lookup = vtfs_lookup
+};
+
+struct file_operations vtfs_dir_ops = {
+    .iterate_shared = vtfs_iterate,
 };
 
 static struct inode *vtfs_get_inode(
@@ -45,6 +50,8 @@ static struct inode *vtfs_get_inode(
     inode->i_gid = GLOBAL_ROOT_GID;
     inode->i_ino = i_ino;
     inode->i_op = &vtfs_inode_ops;
+
+    // inode->i_fop = &vtfs_dir_ops;
 
     return inode;
 }
@@ -87,6 +94,37 @@ struct dentry* vtfs_lookup(
     unsigned int flag            // неиспользуемое значение
 ) {
     return NULL;
+}
+
+int vtfs_iterate(struct file* filp, struct dir_context* ctx) {
+    char fsname[10];
+    struct dentry* dentry = filp->f_path.dentry;
+    struct inode* inode = dentry->d_inode;
+    unsigned long offset = filp->f_pos;
+    int stored = 0;
+    ino_t ino = inode->i_ino;
+
+    unsigned char ftype;
+    ino_t dino;
+    while (true) {
+        if (ino == 100) {
+            if (offset == 0) {
+                strcpy(fsname, ".");
+                ftype = DT_DIR;
+                dino = ino;
+            } else if (offset == 1) {
+                strcpy(fsname, "..");
+                ftype = DT_DIR;
+                dino = dentry->d_parent->d_inode->i_ino;
+            } else if (offset == 2) {
+                strcpy(fsname, "test.txt");
+                ftype = DT_REG;
+                dino = 101;
+            } else {
+                return stored;
+            }
+        }
+    }
 }
 
 static int __init vtfs_init(void) {
