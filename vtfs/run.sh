8c224249f124eba8353b2901d76c0c674b34a450
mip3x <plombiiiri@yandex.ru>
2026-01-17 01:33:59 +0300

    feat: script for testing done

---
+++ vtfs/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Cleaning old build files..."
+sudo make clean
+
+echo "Adding kernel module vtfs..."
+sudo make
+sudo insmod "source/vtfs.ko"
+echo "Mounting kernel module vtfs..."
+sudo mount -t vtfs "TODO" /mnt/vt
+sudo chmod -R 777 /mnt/vt
+
+echo -e
+echo -e
+ls -li /mnt
+cd /mnt/vt
+ls -lia /mnt/vt
+echo -e
+echo -e
+
+echo "Removing previous mount..."
+cd /mnt/vfs
+sudo umount /mnt/vt
+sudo rmmod vtfs
